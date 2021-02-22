626b0b8eeae825b66e9ec72fbd9a5c1cd1947ead
cm-nam <patata123>
2021-02-22 07:22:50 +0100

    Actualizar informes

--- addons/bus/models/res_partner.py
+++ addons/bus/models/res_partner.py
@@ -12,21 +12,8 @@ class ResPartner(models.Model):
 
     @api.multi
     def _compute_im_status(self):
-        self.env.cr.execute("""
-            SELECT
-                U.partner_id as id,
-                CASE WHEN age(now() AT TIME ZONE 'UTC', max(B.last_poll)) > interval %s THEN 'offline'
-                     WHEN age(now() AT TIME ZONE 'UTC', max(B.last_presence)) > interval %s THEN 'away'
-                     ELSE 'online'
-                END as status
-            FROM bus_presence B
-                JOIN res_users U ON B.user_id = U.id
-            WHERE U.partner_id IN %s AND U.active = 't'
-         GROUP BY U.partner_id
-        """, ("%s seconds" % DISCONNECTION_TIMER, "%s seconds" % AWAY_TIMER, tuple(self.ids)))
-        res = dict(((status['id'], status['status']) for status in self.env.cr.dictfetchall()))
         for partner in self:
-            partner.im_status = res.get(partner.id, 'offline')
+            partner.im_status = 'offline'
 
     @api.model
     def im_search(self, name, limit=20):
